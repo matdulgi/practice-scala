1683667e6d5e375bbd44229681e9fd1d5dc25c6d
yura <yrdog9494@gmail.com>
2022-06-10 09:58:52 +0900

    Initial commit

---
+++ src/main/scala/ApplyTest.scala
@@ -0,0 +1,22 @@
+object ApplyTest {
+  def main(args: Array[String]): Unit = {
+    println(ApplyObject(3, 1))
+    val applyObject = new ApplyObject
+    println(applyObject(2, 4))
+  }
+
+}
+
+object ApplyObject {
+  def apply(arg1:Int, arg2:Int): Int= {
+    arg1 + arg2
+  }
+
+}
+class ApplyObject{
+  def apply(arg1:Int, arg2:Int) ={
+    arg1 * arg2
+  }
+
+}
+
